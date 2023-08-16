df482f8532e37072052ca7029239ddcaea374002
Lukasz Zajaczkowski <zreigz@gmail.com>
2023-08-16 17:29:28 +0200

    set x11

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/rivo/tview"
+	"runtime/debug"
 )
 
 func main() {
 	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
 	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+		debug.PrintStack()
 		panic(err)
 	}
 }
